eb060838811b4d30fd39450da73ca3a788c69c54
kwongfung <skytrc@163.com>
2023-03-18 23:50:02 +0800

    feat:uniswap v2 part2 code & part2 doc

--- src/uniswap/v2/MyuniswapV2Pair.sol
+++ src/uniswap/v2/MyuniswapV2Pair.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -10,11 +11,17 @@ interface IERC20 {
     function transfer(address to, uint256 amount) external;
 }
 
+error BalanceOverflow();
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidK();
 error TransferFailed();
 
 contract MyuniswapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
+
     uint256 constant MINIMUM_LIQUIDITY = 1000;
 
     address public token0;
@@ -23,9 +30,20 @@ contract MyuniswapV2Pair is ERC20, Math {
     uint112 private reserve0;
     uint112 private reserve1;
 
-    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address to);
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
+    event Swap(
+        address indexed sender,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
 
     constructor(address token0_, address token1_)
         ERC20("Myuniswap-V2", "MyuniV2", 18)
@@ -45,8 +63,10 @@ contract MyuniswapV2Pair is ERC20, Math {
 
         if(totalSupply == 0) {
             liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            // 确保始终有足够的流动性
             _mint(address(0), MINIMUM_LIQUIDITY);
         } else {
+            // 计算出价值最低的token数量来计算LP-token
             liquidity = Math.min(
                 (amount0 * totalSupply) / _reserve0,
                 (amount1 * totalSupply) / _reserve1
@@ -59,15 +79,15 @@ contract MyuniswapV2Pair is ERC20, Math {
 
         _mint(msg.sender, liquidity);
 
-        _update(balance0, balance1);
+        _update(balance0, balance1, _reserve0, _reserve1);
 
         emit Mint(msg.sender, amount0, amount1);
     }
 
-    function burn(address to) public {
+    function burn() public {
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
-        uint256 liquidity = balanceOf[to];
+        uint256 liquidity = balanceOf[msg.sender];
 
         uint256 amount0 = (liquidity * balance0) / totalSupply;
         uint256 amount1 = (liquidity * balance1) / totalSupply;
@@ -76,26 +96,72 @@ contract MyuniswapV2Pair is ERC20, Math {
             revert InsufficientLiquidityBurned();
         }
 
-        _burn(to, liquidity);
+        // 销毁LP-tokens
+        _burn(msg.sender, liquidity);
 
-        _saferTransfer(token0, to, amount0);
-        _saferTransfer(token1, to, amount1);
+        // 安全转移代币回之前的流动性提供者
+        _saferTransfer(token0, msg.sender, amount0);
+        _saferTransfer(token1, msg.sender, amount1);
 
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
 
-        _update(balance0, balance1);
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        _update(balance0, balance1, reserve0_, reserve1_);
 
-        emit Burn(msg.sender, amount0, amount1, to);
+        emit Burn(msg.sender, amount0, amount1);
+    }
+
+    function swap(
+        uint256 amount0Out, 
+        uint256 amount1Out,
+        address to
+    ) public {
+        // 判断输入的数量不等于0
+        if(amount0Out == 0 && amount1Out == 0) {
+            revert InsufficientOutputAmount();
+        }
+
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        // 判断代币存量是否足够
+        if(amount0Out > reserve0_ || amount1Out > reserve1_) {
+            revert InsufficientLiquidity();
+        }
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        // 保证互换后准备金的乘积必须等于或大于互换前的乘积
+        if(balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_)) {
+            revert InvalidK();
+        }
+
+        // 更新存量
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        // 代币转账操作
+        if(amount0Out > 0) {
+            _saferTransfer(token0, to, amount0Out);
+        }
+        if(amount1Out > 0) {
+            _saferTransfer(token1, to, amount1Out);
+        }
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+
     }
 
     function sync() public {
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
         _update(
             IERC20(token0).balanceOf(address(this)),
-            IERC20(token1).balanceOf(address(this))
+            IERC20(token1).balanceOf(address(this)),
+            reserve0_,
+            reserve1_
         );
     }
 
+    // 获取代币存储量和上次交换的时间
     function getReserves()
         public
         view
@@ -105,16 +171,39 @@ contract MyuniswapV2Pair is ERC20, Math {
             uint32
         )
     {
-        return (reserve0, reserve1, 0);
+        return (reserve0, reserve1, blockTimestampLast);
     }
 
-    function _update(uint256 balance0, uint256 balance1) private {
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max)
+            revert BalanceOverflow();
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                    timeElapsed;
+            }
+        }
+
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
 
         emit Sync(reserve0, reserve1);
     }
 
+    // 保证转账后获得提示
     function _saferTransfer(
         address token,
         address to,
--- src/uniswap/v2/READEME.md
+++ src/uniswap/v2/READEME.md
@@ -47,8 +47,6 @@ contract ZuniswapV2Pair is ERC20, Math {
 
 $Liquidity_{minted} = \sqrt{Amount_0 * Amount_1}$
 
-
-
 当流动池已经存在流动性时，应该如何计算？
 
 在V1中，公式如下
@@ -130,10 +128,206 @@ function burn() public {
 }
 ```
 
-
-
-
-
 如果是仔细的看过V1就会知道错在哪里了。因为`Pair`合约一般是由`Router`合约控制，这里的`msg.sender`的指向`Router`合约，所以只要添加参数`address to`替换掉`msg.sender`即可
 
+## V2 part2
 
+上一部分，我们完成了添加和移除流动性。那么拥有流动性以后，我们就可以编写`swap`的部分。
+
+### Swap
+
+回看V1的`Exchange`合约，我们把计算`token`交换的数量和转移`token`的函数放在了一起。但是在V2中，我们就应该把他们对应的职责拆分。就像之前所说的，我们`Router`面向和处理用户的交易，再**调用**`Pair`的逻辑完成转账。
+
+所以在`Pair`中，我们只需要根据给出的两种`token`数量来完成交易即可，计算部分放在`Router`。
+
+另外根据恒定乘积模型**保证互换后准备金的乘积必须等于或大于互换前的乘积**。
+
+简单点理解如果用户进行的交换导致准备金乘积**变小**，那么交换比率也会**变大**，从而导致用户需要**支付更多的代币**才能得到想要的代币。为了避免这种情况，Uniswap要求交换前后的准备金乘积**保持不变或增大**，从而保证交易的**公平性**和**流动性**。
+
+1. 判断输入的数量不等于0
+
+2. 获取两种代币存量
+
+3. 判断代币存量足够
+
+4. 计算交换后，两种代币`Pair`的余额
+
+5. 保证互换后准备金的乘积必须等于或大于互换前的乘积
+
+6. 更新存量
+
+7. 转账
+
+```solidity
+function swap(
+        uint256 amount0Out, 
+        uint256 amount1Out,
+        address to
+    ) public {
+        // 判断输入的数量不等于0
+        if(amount0Out == 0 && amount1Out == 0) {
+            revert InsufficientOutputAmount();
+        }
+
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        // 判断代币存量是否足够
+        if(amount0Out > reserve0_ || amount1Out > reserve1_) {
+            revert InsufficientLiquidity();
+        }
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        // 保证互换后准备金的乘积必须等于或大于互换前的乘积
+        if(balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_)) {
+            revert InvalidK();
+        }
+
+        // 更新存量
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        // 代币转账操作
+        if(amount0Out > 0) {
+            _saferTransfer(token0, to, amount0Out);
+        }
+        if(amount1Out > 0) {
+            _saferTransfer(token1, to, amount1Out);
+        }
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+
+    }
+```
+
+### 重入攻击
+
+关于重入攻击可以看[重入攻击讲解](../../hacks/reentrancy/README.md)
+
+这里就讲讲`Swap`函数中怎么防止重入攻击
+
+1. 在所以状态变量更新后，调用`call`函数完成转账
+
+2. 使用`revert`，当发生错误时，回滚交易
+
+
+
+### 价格预言
+
+预言机的本意是，**连接即连接区块链和链外服务的桥梁**，以便可以从智能合约中查询真实世界的数据。
+
+Uniswap虽然是链上应用，也可以作为一个预言机，因为它是截至到目前为止最大的去中心化交易所。交易者可以通过获取Uniswap中的各个代币价格，对不同的去中心交易所进行跨交易所套利。
+
+V2中的提供的价格预言被成为**时间加权平均价格**，它可以获得两个时间点之间的平均价格。
+
+为了完成价格预言。我们要存储累积价格，在每次交换之前，它会计算当前的边际价格（不包括手续费），将其乘以自上次交换以来经过的秒数，并将该数字加到上一个数字中。
+
+我们可知价格公式：
+
+$price_0 = \frac{reserve_1}{resever_0}$
+
+$price_1 = \frac{reserve_0}{resever_1}$
+
+由于Solidity不提供浮点运算，在计算该类价格时运算可以能出现小数，所以我们需要提高精度，Uniswap使用**UQ112.112number**来解决。
+
+**UQ112.112number**指的是一个数字，它的小数部分使用112位（2**112），选择112位是为了使储备状态变量的存储更优化。后面回解释。
+
+
+
+首先我们需要存储上一次更新价格时区块时间
+
+```solidity
+uint32 private blockTimestampLast;
+```
+
+再更新价格函数中中，我们需要扩大精度，再进行运算，最后再更新价格。
+
+```solidity
+function _update(
+    uint256 balance0,
+    uint256 balance1,
+    uint112 reserve0_,
+    uint112 reserve1_
+) private {
+    ...
+    unchecked {
+        uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+        if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+            price0CumulativeLast +=
+                uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                timeElapsed;
+            price1CumulativeLast +=
+                uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                timeElapsed;
+        }
+    }
+
+    reserve0 = uint112(balance0);
+    reserve1 = uint112(balance1);
+    blockTimestampLast = uint32(block.timestamp);
+
+    ...
+}
+```
+
+这里为什么会用到`unchecked`后面也会解释
+
+
+
+### 存储优化
+
+为什么会用到`uint112`而不是`uint256`。这就是要从EVM说起。
+
+因为EVM中的每个操作都需要消耗一定的gas。特别是存储`SSTORE`和`SLOAD`，每次操作都是以一个**slot**（插槽）为单位进行操作，每个插槽为32字节。而`uint256`类型变量正好占有32字节，也就是一个uint256类型完全储存在一个插槽上。
+
+如果我们采用`uint256`作为小数位运算，再计算上整数位，占用的插槽就大于等2。
+
+而采用两个`uint112`表示小数部分和整数部分，两个加来为224。能确保两部分无论实在储存还是加载都在一个插槽上，节省了gas。
+
+
+
+### 整数上溢和下溢
+
+我们在`_update`时使用了`unchecked`，这又是为什么呢？
+
+我们先看回`uint256`的上限和下限问题
+
+$uint256(2^{256} - 1) + 1 = 0$
+
+$uint256(0) - 1 = 2^{256} - 1$
+
+在**solidity 0.8.0**前，是不会检查溢出的问题，所以当时就有了library`SafeMath`。但是安全检查又会消耗很多的gas，而且运算时间变慢。
+
+回到代码中，无论是时间间隔的运算还是累积价格时，溢出的边界都是可知的。所以可以使用`unchecked`
+
+
+
+### 安全转账
+
+我们可以注意到`Pair`中有一个`_safeTransfer`
+
+```solidity
+function _saferTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) private {
+        (bool success, bytes memory data) = token
+            .call(abi.encodeWithSignature("transfer(address,uint256)", to, value)
+        );
+
+        if(!success || data.length != 0 && !abi.decode(data, (bool))) {
+            revert TransferFailed();
+        }
+    }
+```
+
+为什么不直接使用ERC20的`transfer`接口呢？
+
+因为有一些ERC20的`transfer`接口无论是否成功，都不会返回结果，你无从得知是否转账成功。
+
+而使用`call`都会返回结果，让我们更容易的去判断。
+
+---
+
+最后讲一讲测试用例方面。**juiwen**大佬写了很多测试用例，到了part2测试文件已经到了400多行，因为他要确保每个函数都能稳定运行，会从很多方面去编写测试用例。有兴趣的可以自己去查看，而我认为只要跑通他的用例就算完成，也不需要一行一行的去解读（不排除以后会去做）。
