43da109246b0604cf42646a3cb1b3103d0ff6bf2
kwongfung <skytrc@163.com>
2023-03-27 23:25:44 +0800

    feat:oracle

---
+++ script/oracle/Oracle.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "../../src/oracle/GetPrice.sol";
+
+contract OracleScript is Script {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        
+        GetPrice getPrice = GetPrice(this);
+
+        vm.stopBroadcast();
+        
+    }
+}
--- src/oracle/GetPrice.sol
+++ src/oracle/GetPrice.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract GetPrice {
 
-    
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: BTC/USD
+     * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+    }
+
+    function getPrice() public view returns (int256) {
+        ( , int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
 }
